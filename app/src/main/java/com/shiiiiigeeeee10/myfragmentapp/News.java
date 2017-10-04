b28c038fa6f9006235eb9083efa2665577306b35
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-10-04 17:17:59 +0900

    create news class

---
+++ app/src/main/java/com/shiiiiigeeeee10/myfragmentapp/News.java
@@ -0,0 +1,22 @@
+package com.shiiiiigeeeee10.myfragmentapp;
+
+/**
+ * Created by shigeru on 2017/10/04.
+ */
+
+public class News {
+    public final static String[] Titles = {
+            "title 0",
+            "title 1",
+            "title 2",
+            "title 3",
+            "title 4"
+    }
+    public final static String[] Details = {
+            "Detail 0",
+            "Detail 1",
+            "Detail 2",
+            "Detail 3",
+            "Detail 4"
+    }
+}
